a994cd380008b73cd4bea458e04658598269e334
Florian Tranier <tranierflorian@gmail.com>
2024-04-27 10:08:10 +0000

    bump version to 1.3.4

---
+++ src/patchnotes/1.3.4.ts
@@ -0,0 +1,22 @@
+export const markdownNote = `## Mise à jour 1.3.4
+
+### Améliorations
+
+- Usage des mentions de channel au lieu des invitations pour le channel vocal
+
+### Tech
+
+- Bump discord.js version vers 14.14.1
+
+================
+
+## Update 1.3.4
+
+### Improvements
+
+- Use channel mentions instead of invitations for voice channels
+
+### Tech
+
+- Bump discord.js version to 14.14.1
+`
