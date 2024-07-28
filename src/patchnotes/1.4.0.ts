d53f5d7e05581ea6c0a2a0e96086790ee6c3c905
Florian Tranier <tranierflorian@gmail.com>
2024-07-28 13:36:44 +0000

    patchnote 1.4.0

---
+++ src/patchnotes/1.4.0.ts
@@ -0,0 +1,26 @@
+export const markdownNote = `## Mise à jour 1.4.0
+
+### Fonctionnalités
+
+- Traduction des commandes /search et /event en français
+
+Création d'événements (BETA) :
+
+- Les joueurs/joueuses peuvent créer des événements en utilisant la commande /event
+- L'événement est créé dans le système d'événement natif au serveur Discord
+- Vous pouvez choisir un channel d'annonce pour notifier les membres du serveur (optionnel sur la commande /event, si vous avez les permissions de poster dans le channel)
+
+================
+
+## Update 1.4.0
+
+### Features
+
+- Translation of the /search and /event commands into French
+
+Event creation (BETA):
+
+- Players can create events using the /event command
+- The event is created in the native event system on the Discord server
+- You can choose an announcement channel to notify server members (optional on the /event command, if you have permission to post in the channel)
+`
